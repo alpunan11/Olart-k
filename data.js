f1753ef605b986ca25d21d77ba90752d8335b493
alpunan11 <eminalp_2001@hotmail.com>
2025-06-14 23:40:01 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+export const exercises = {
+  1: [
+    { name: "Squat Jump", reps: "3x10" },
+    { name: "Wall Sit", reps: "3x30sn" },
+  ],
+  2: [
+    { name: "Lunge Jump", reps: "3x8" },
+    { name: "Calf Raise", reps: "3x15" },
+  ],
+  3: [
+    { name: "Burpee", reps: "3x10" },
+    { name: "Step Up", reps: "3x12" },
+  ],
+  4: [
+    { name: "Glute Bridge", reps: "3x15" },
+    { name: "Mountain Climbers", reps: "3x20sn" },
+  ],
+  5: [
+    { name: "Jumping Jack", reps: "3x30sn" },
+    { name: "Side Lunge", reps: "3x12" },
+  ],
+  6: [
+    { name: "Tuck Jump", reps: "3x8" },
+    { name: "Single Leg Calf Raise", reps: "3x10 (her bacak)" },
+  ],
+  7: [
+    { name: "Box Jump (step yerine yapılabilir)", reps: "3x6" },
+    { name: "Skater Jump", reps: "3x12" },
+  ],
+};
