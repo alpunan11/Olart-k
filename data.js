c9aff60b0c5f68abb2838524f0da3595c192b99e
alpunan11 <eminalp_2001@hotmail.com>
2025-06-14 23:36:58 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+export const exercises = {
+  1: [
+    { name: "Squat Jump", reps: "3x10" },
+    { name: "Wall Sit", reps: "3x30sn" },
+  ],
+  2: [
+    { name: "Lunge Jump", reps: "3x8" },
+    { name: "Calf Raise", reps: "3x15" },
+  ],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+};
